dd742750fb56189ebe8b4fdc1571890acce50394
Mark Chen <67337568+mc384@users.noreply.github.com>
2022-11-04 17:37:02 -0400

    Update 509_Fibonacci_Number.py

--- 509_Fibonacci_Number.py
+++ 509_Fibonacci_Number.py
@@ -1,3 +1,11 @@
+# Recursion
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+
 # Tabulation
 class Solution:
     def fib(self, n: int) -> int:
