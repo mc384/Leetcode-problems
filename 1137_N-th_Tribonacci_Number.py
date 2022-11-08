52e9d069f15606f6c27a98c2c5e722c81700efa4
Mark Chen <67337568+mc384@users.noreply.github.com>
2022-11-08 18:29:57 -0500

    Update 1137_N-th_Tribonacci_Number.py

--- 1137_N-th_Tribonacci_Number.py
+++ 1137_N-th_Tribonacci_Number.py
@@ -1,19 +1,12 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1 or n == 2:
-            return 1
-        t0 = 0 
-        t1 = 1
-        t2 = 1
-        res = 0 
+        if n == 0: return 0
+        if n == 1 or n == 2: return 1
+        t0, t1, t2, res = 0, 1, 1, 0
 
         for i in range(3, n+1):
             res = t0 + t1 + t2 
-            t0 = t1 
-            t1 = t2 
-            t2 = res 
+            t0, t1, t2 = t1, t2, res 
         return res
         # Time: O(n)
         # Space: O(1)
