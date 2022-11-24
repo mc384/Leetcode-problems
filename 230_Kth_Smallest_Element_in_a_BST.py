f07dc1964d954a24905266f7ee0321dc737a3e25
Mark Chen <67337568+mc384@users.noreply.github.com>
2022-11-23 21:07:22 -0500

    Create 230_Kth_Smallest_Element_in_a_BST.py

---
+++ 230_Kth_Smallest_Element_in_a_BST.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+
+        def dfs(root):
+            if root.left:
+                dfs(root.left)
+            stack.append(root.val)
+            if root.right:
+                dfs(root.right)
+        dfs(root)
+        return stack[k-1]
