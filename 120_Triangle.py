a68740b3fd13f927e43751335aa7689b247c36b1
Mark Chen <67337568+mc384@users.noreply.github.com>
2022-12-02 18:35:07 -0500

    Create 120_Triangle.py

---
+++ 120_Triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])-1, -1, -1):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+
+        return triangle[0][0]
+      # Time: O(n^2)
+      # Space: O(1)
