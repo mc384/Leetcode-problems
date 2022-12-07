5e2bac0c7f9cb532b1d046446774e37e7cd3d141
Mark Chen <67337568+mc384@users.noreply.github.com>
2022-12-07 17:57:45 -0500

    Create 771_Jewels_and_Stones.py

---
+++ 771_Jewels_and_Stones.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        res = 0
+        jewel = {}
+
+        for i in jewels:
+            jewel[i] = 1 + jewel.get(i, 0)
+        
+        for j in stones:
+            try:
+                if jewel[j]:
+                    res += 1
+            except KeyError:
+                continue
+        return res 
