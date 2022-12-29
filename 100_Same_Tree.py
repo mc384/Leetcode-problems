d5d522403e41f3d0be6ccdfd9f44248cd20aa07b
Mark Chen <67337568+mc384@users.noreply.github.com>
2022-12-29 12:59:18 -0500

    Update 100_Same_Tree.py

--- 100_Same_Tree.py
+++ 100_Same_Tree.py
@@ -9,22 +9,12 @@
 ## Depth-first search
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p:
-            if not q:
-                return True
-            else:
-                return False
-        if not q:
-            if not p:
-                return True
-            else:
-                return False
-        if p.val != q.val:
+        if not p and not q:
+            return True  
+        if (p and not q) or (not p and q):
             return False
+        if p.val != q.val:
+            return False 
         else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
-## Test Cases
-# isSameTree([], []) returns True
-# isSameTree([1,2,3,4], [1,2,3]) returns False
-# isSameTree([1, null, null], [1, null, null]) returns True
+
